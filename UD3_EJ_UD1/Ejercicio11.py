54b80f8006ed0ddf232283cb08619763577a3c53
Javier García <tahuria@alumnos.alborfp.com>
2025-10-30 18:20:27 +0100

    mayor y menor

---
+++ UD3_EJ_UD1/Ejercicio11.py
@@ -0,0 +1,9 @@
+
+num1 = float(input("Introduce el primer número: "))
+num2 = float(input("Introduce el segundo número: "))
+
+if num1 > num2:
+    print("El número mayor es:", num1)
+elif num2 > num1:
+    print("El número mayor es:", num2)
+
