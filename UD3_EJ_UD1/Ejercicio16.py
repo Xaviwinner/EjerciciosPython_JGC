7738c534bb0a12eedf10be7b93d7f13f477a21b4
Javier García <tahuria@alumnos.alborfp.com>
2025-10-30 19:44:05 +0100

    cifras

--- UD3_EJ_UD1/Ejercicio16.py
+++ UD3_EJ_UD1/Ejercicio16.py
@@ -2,4 +2,4 @@ numero = int(input("Introduce número entre 0 y 99999: "))
 
 if 0 <= numero <= 99999:
     cifras = len(str(numero))
-    print("El número tiene", cifras, "cifras.")
+    print("El número tiene"+ cifras +"cifras.")
