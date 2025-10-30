150532ea4a0e1eb5005bda145ac567801a3baab2
Javier García <tahuria@alumnos.alborfp.com>
2025-10-30 18:23:58 +0100

    orden numero

---
+++ UD3_EJ_UD1/Ejercicio13.py
@@ -0,0 +1,7 @@
+num1 = float(input("Introduce el primer número: "))
+num2 = float(input("Introduce el segundo número: "))
+
+if num1 < num2:
+    print("Los números en orden ascendente son:", num1, "y", num2)
+elif num2 < num1:
+    print("Los números en orden ascendente son:", num2, "y", num1)
