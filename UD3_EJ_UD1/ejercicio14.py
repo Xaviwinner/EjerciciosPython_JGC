aa534716895217f81bd72651aa1d8c1719e2d79b
unknown <tahuria@alumnos.alborfp.com>
2025-11-07 09:51:05 +0100

    exercise14

---
+++ UD3_EJ_UD1/ejercicio14.py
@@ -0,0 +1,16 @@
+primero = int(input("Ingrese el primero:"))
+segundo = int(input("Ingrese el segundo:"))
+
+
+suma= primero +segundo
+resta= primero -segundo
+
+producto=primero*segundo
+
+division=primero/segundo
+
+print("suma: " + suma)
+print("resta: " + resta)
+print("producto: " + producto)
+print("division: "+ division)
+
