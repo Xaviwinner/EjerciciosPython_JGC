c23f78262a7f59a8b0a309a7b864bec1218fc73e
unknown <tahuria@alumnos.alborfp.com>
2025-11-10 09:43:51 +0100

    15exercise

---
+++ UD3_EJ_UD1/ejercicio15.py
@@ -0,0 +1,9 @@
+"""programa que lee dos números y muestra el mayor"""
+
+primero = int(input("Ingrese el primero:"))
+segundo = int(input("Ingrese el segundo:"))
+
+if primero > segundo:
+    print("el peimero es mayor")
+else:
+    print("el segundo es el mayor")
