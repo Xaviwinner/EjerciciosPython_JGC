766e60395d41a5345a7600bd3f51297f614812bc
unknown <tahuria@alumnos.alborfp.com>
2025-11-10 09:45:52 +0100

    exercise16

---
+++ UD3_EJ_UD1/ejercicio16.py
@@ -0,0 +1,9 @@
+"""programa que lea un número y dice si es positivo o negativo, consideramos el cero como positivo."""
+
+
+primero = int(input("Ingrese el primero:"))
+
+if primero >= 0:
+    print("el numero es positivo")
+else:
+    print("el numero es negativo")
