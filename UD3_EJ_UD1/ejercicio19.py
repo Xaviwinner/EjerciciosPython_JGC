50db8ad002fa30cea2d34816f3ba55842d384d04
unknown <tahuria@alumnos.alborfp.com>
2025-11-14 09:19:49 +0100

    exercise19

--- UD3_EJ_UD1/ejercicio19.py
+++ UD3_EJ_UD1/ejercicio19.py
@@ -1,6 +1,3 @@
-"""lea tres números y nos diga cual es mayor, cual menor y cuales son iguales"""
-
 primero = int(input("Ingrese el primero:"))
 segundo = int(input("Ingrese el segundo:"))
 tercero = int(input("Ingrese el tercero:"))
-
