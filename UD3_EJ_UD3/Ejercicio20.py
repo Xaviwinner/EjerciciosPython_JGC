30d5a162095c72bd2a874571e83f859164090b81
unknown <tahuria@alumnos.alborfp.com>
2025-11-17 10:02:49 +0100

    exercise20

---
+++ UD3_EJ_UD3/Ejercicio20.py
@@ -0,0 +1,19 @@
+numero = int(input("Introduce la nota"))
+
+if numero<3:
+    print("muy deficiente")
+elif 3<numero<5:
+    
+    print("insuficiente")
+elif 5<=numero<6:
+    print("suficiennmte")
+
+
+elif 6<=numero<7:
+    print("bien")
+
+elif 7<=numero<9:
+    print("notable")
+
+elif 9<=numero<10:
+    print("sobresaliente")
