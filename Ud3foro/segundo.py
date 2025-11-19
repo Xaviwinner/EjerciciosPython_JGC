186365de4bdd651bb368755b660de0d0943db990
javier <garcia@gmail.com>
2025-11-19 20:17:19 +0100

    exerciseforo

---
+++ Ud3foro/segundo.py
@@ -0,0 +1,16 @@
+espacio = " "
+numero = "4"
+
+n = int(input("Introduce un número: "))
+print(numero)
+for i in range(1, n-1):
+    print(numero + espacio * i, numero)
+print((numero + espacio) * n)
+
+
+
+
+
+
+
+
