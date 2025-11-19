b18b494d5074f257bc0e0d4b9c8cc404365e23b8
unknown <tahuria@alumnos.alborfp.com>
2025-11-19 14:19:25 +0100

    exercise

--- Ud3foro/primero.py
+++ Ud3foro/primero.py
@@ -1,13 +1,10 @@
-
-espacio=" "
-asteriscos="*"
-
-
-numero = int(input("Introduce un numero"))
+espacio = " "
+asteriscos = "*"
 
 
-
-
+numero = int(input("Introduce un numero: "))
+for i in range(1, numero + 1):
+    print(espacio* (numero + 0), asteriscos)
 
 
 
