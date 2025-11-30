0c78b36295373cdb95126a57c0e4fc6d5a23eae5
javier <garcia@gmail.com>
2025-11-30 19:46:59 +0100

    ejercicios2

--- Ud3foro/EjercicioCuadradoDiagonales.py
+++ Ud3foro/EjercicioCuadradoDiagonales.py
@@ -5,7 +5,7 @@ numero=int(input("escribe un numero"))
 print(asterisco*numero)
 for i in range(1,numero//2):
     print((asterisco)+(espacio*i+asterisco)+espacio*((numero//2)-cont)+asterisco)
-    cont=cont+1  #+(espacio*numero-i)+asterisco
+    cont=cont+1  
 print(asterisco+espacio*(numero//2)+asterisco+espacio+asterisco)
 for j in range(1,numero//2):
     print((asterisco)+(espacio*((numero//2)-j)+asterisco))
--- Ud3foro/M.py
+++ Ud3foro/M.py
@@ -3,10 +3,15 @@ espacios=" "
 numero=int(input("escribe un numero"))
 print(asteriscos+(espacios*(numero))+asteriscos)
 for i in range(1,numero//2+1):
-    if(i==numero//2):
-        print(asteriscos+espacios*(numero//2)+asteriscos+espacios*(numero//2)+asteriscos)
+    if(numero%2==0):
+        if(i==numero//2):
+            print(asteriscos+espacios*(numero//2-1)+asteriscos+espacios*(numero//2)+asteriscos)
+        else:
+            print((asteriscos+(espacios*i)+asteriscos)+(espacios*(numero - 2 - 2*i)+asteriscos)+espacios*i+asteriscos)
     else:
-        print((asteriscos+(espacios*i)+asteriscos)+(espacios*(numero - 2 - 2*i)+asteriscos)+espacios*i+asteriscos)
-
+        if(i==numero//2):
+            print(asteriscos+espacios*(numero//2)+asteriscos+espacios*(numero//2)+asteriscos)
+        else:
+            print((asteriscos+(espacios*i)+asteriscos)+(espacios*(numero - 2 - 2*i)+asteriscos)+espacios*i+asteriscos)
 for j in range (1,numero//2+1):
     print(asteriscos+espacios*numero+asteriscos)
