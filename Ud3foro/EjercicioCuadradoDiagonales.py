c099989956e68cc200570f963d8db8dd4eeb8856
javier <garcia@gmail.com>
2025-11-30 13:13:10 +0100

    ejerciciosforo

---
+++ Ud3foro/EjercicioCuadradoDiagonales.py
@@ -0,0 +1,14 @@
+asterisco="*"
+espacio=" "
+cont=0
+numero=int(input("escribe un numero"))
+print(asterisco*numero)
+for i in range(1,numero//2):
+    print((asterisco)+(espacio*i+asterisco)+espacio*((numero//2)-cont)+asterisco)
+    cont=cont+1  #+(espacio*numero-i)+asterisco
+print(asterisco+espacio*(numero//2)+asterisco+espacio+asterisco)
+for j in range(1,numero//2):
+    print((asterisco)+(espacio*((numero//2)-j)+asterisco))
+
+
+print(asterisco*numero)
